91ed8bcd984b48f516ddbcef8852e18051adce22
Jeffrey <kingtingthegreat@gmail.com>
2025-01-16 12:56:57 -0500

    add weights and question numbers

--- uploaded_links.py
+++ uploaded_links.py
@@ -1,6 +1,7 @@
 import unittest
 from urllib.parse import urlparse
 from requests import get
+from gradescope_utils.autograder_utils.decorators import weight, number
 
 vercel_domain = "vercel.app"
 github_domain = "github.com"
@@ -14,11 +15,15 @@ class UploadedLinks(unittest.TestCase):
         self.vercel_link = "https://cs391-url-shortener.vercel.app/"
         self.github_link = "https://github.com/KingTingTheGreat/top-fetch"
 
+    @weight(50)
+    @number(1)
     def test_vercel_link(self):
         parsed_url = urlparse(self.vercel_link)
         self.assertEqual(parsed_url.hostname, vercel_domain)
         self.assertTrue(is_valid_url(self.vercel_link))
 
+    @weight(50)
+    @number(1)
     def test_github_link(self):
         parsed_url = urlparse(self.github_link)
         self.assertEqual(parsed_url.hostname, github_domain)
