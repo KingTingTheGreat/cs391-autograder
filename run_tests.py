4363227b000356445244a4ffee744ff1be6fe3e2
Jeffrey <kingtingthegreat@gmail.com>
2025-01-16 13:15:26 -0500

    fix path

--- run_tests.py
+++ run_tests.py
@@ -4,6 +4,6 @@ from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 if __name__ == "__main__":
     print("discovering tests")
     suite = unittest.defaultTestLoader.discover("tests")
-    with open("autograder/results/results.json", "w") as f:
+    with open("/autograder/results/results.json", "w") as f:
         print("writing to output")
         JSONTestRunner(visibility="visible", stream=f).run(suite)
