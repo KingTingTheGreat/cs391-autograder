18bbcfa1873c0cfb80aab944823378cdc6c565e6
Jeffrey <kingtingthegreat@gmail.com>
2025-01-16 13:23:27 -0500

    logging

--- run_tests.py
+++ run_tests.py
@@ -2,8 +2,6 @@ import unittest
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 if __name__ == "__main__":
-    print("discovering tests")
     suite = unittest.defaultTestLoader.discover("tests")
     with open("/autograder/results/results.json", "w") as f:
-        print("writing to output")
         JSONTestRunner(visibility="visible", stream=f).run(suite)
--- tests/test_uploaded_links.py
+++ tests/test_uploaded_links.py
@@ -8,29 +8,29 @@ github_domain = "github.com"
 
 def is_valid_url(url) -> bool:
     res = get(url)
-    print(res.text)
+    if res.ok:
+        print("url works")
+    else:
+        print("url does not work")
     return res.ok
 
 class TestUploadedLinks(unittest.TestCase):
     def setUp(self):
-        print("setting up UploadedLinks")
         self.vercel_link = "https://cs391-url-shortener.vercel.app/"
         self.github_link = "https://github.com/KingTingTheGreat/top-fetch"
 
     @weight(50)
     @number(1)
     def test_vercel_link(self):
-        print("test vercel link")
         parsed_url = urlparse(self.vercel_link)
         self.assertIsNot(parsed_url.hostname, None)
         if parsed_url.hostname is not None:
-            self.assertTrue(parsed_url.hostname.endswith(vercel_domain))
-        self.assertTrue(is_valid_url(self.vercel_link))
+            self.assertTrue(parsed_url.hostname.endswith(vercel_domain), msg="checking vercel link")
+        self.assertTrue(is_valid_url(self.vercel_link), msg="url works")
 
     @weight(50)
     @number(1)
     def test_github_link(self):
-        print("test github link")
         parsed_url = urlparse(self.github_link)
-        self.assertEqual(parsed_url.hostname, github_domain)
-        self.assertTrue(is_valid_url(self.github_link))
+        self.assertEqual(parsed_url.hostname, github_domain, msg="Checking GitHub link")
+        self.assertTrue(is_valid_url(self.github_link), msg="url works")
