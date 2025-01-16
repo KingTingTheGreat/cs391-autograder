060effce693f63d857b576472e67651022115405
Jeffrey <kingtingthegreat@gmail.com>
2025-01-16 13:13:39 -0500

    lalala

--- tests/test_uploaded_links.py
+++ tests/test_uploaded_links.py
@@ -10,14 +10,16 @@ def is_valid_url(url) -> bool:
     res = get(url)
     return res.ok
 
-class UploadedLinks(unittest.TestCase):
+class TestUploadedLinks(unittest.TestCase):
     def setUp(self):
+        print("setting up UploadedLinks")
         self.vercel_link = "https://cs391-url-shortener.vercel.app/"
         self.github_link = "https://github.com/KingTingTheGreat/top-fetch"
 
     @weight(50)
     @number(1)
     def test_vercel_link(self):
+        print("test vercel link")
         parsed_url = urlparse(self.vercel_link)
         self.assertEqual(parsed_url.hostname, vercel_domain)
         self.assertTrue(is_valid_url(self.vercel_link))
@@ -25,6 +27,7 @@ class UploadedLinks(unittest.TestCase):
     @weight(50)
     @number(1)
     def test_github_link(self):
+        print("test github link")
         parsed_url = urlparse(self.github_link)
         self.assertEqual(parsed_url.hostname, github_domain)
         self.assertTrue(is_valid_url(self.github_link))
